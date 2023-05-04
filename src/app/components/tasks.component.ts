97e83ca8b63adfb646ea128a59ff68c410585ed4
ridzy <ridzuanazmi.11@gmail.com>
2023-05-05 05:23:21 +0800

    Completed day 32

--- src/app/components/tasks.component.ts
+++ src/app/components/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { Todo } from '../models/todo';
 import { Subject } from 'rxjs';
 
